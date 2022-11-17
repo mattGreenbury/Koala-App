b89797e6fb3fa30dc916d34a5e125b5fbce2a5fe
Matt Greenbury <matt.d.greenbury@gmail.com>
2022-11-18 00:33:09 +1000

    added files togo live

--- api/backend/khome/settings.py
+++ api/backend/khome/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wf#_91vv*j*g*cnj0fr8!wdn*p3ru^%$vuol-fx6h&on$b0*wa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -128,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
