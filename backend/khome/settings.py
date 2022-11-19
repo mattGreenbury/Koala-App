6ed1c5517c9a5ba911b2b7e96bcf8463967b4ebc
Matt Greenbury <matt.d.greenbury@gmail.com>
2022-11-19 14:12:18 +1000

    Update settings.py

--- backend/khome/settings.py
+++ backend/khome/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wf#_91vv*j*g*cnj0fr8!wdn*p3ru^%$vuol-fx6h&on$b0*wa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
